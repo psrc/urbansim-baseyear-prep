f305246312199ed4c11365dc0130dc3eace50717
Christy Lam <cyyllam@gmail.com>
2018-11-26 09:33:29 -0800

    revised to block level hh totals

--- imputation/data2017/read_hh_totals.R
+++ imputation/data2017/read_hh_totals.R
@@ -2,7 +2,21 @@
 # county_id, tract, block_group, HH
 
 library(data.table)
-data.dir <- "C:/Users/CLam/Desktop/urbansim-baseyear-prep/imputation/data2017"
+library(stringr)
+
+# data.dir <- "C:/Users/CLam/Desktop/urbansim-baseyear-prep/imputation/data2017" # remove when committing
 hhtots <- read.table(file.path(data.dir, "OFMPopHHblocks.csv"), sep=",", header=TRUE)[, c("COUNTYFP10", "GEOID10", "HU2017")]
 colnames(hhtots)[3] <- "HH"
 hhtots <- data.table(hhtots)
+
+hhtots[, GEOID10 := as.character(GEOID10)
+       ][, `:=`(county_id = COUNTYFP10, 
+                tract = str_sub(GEOID10, start = 6, end = 11), 
+                block_group = str_sub(GEOID10, start = 12, end = 12), 
+                block = str_sub(GEOID10, start = 12, end = 15))
+         ][, COUNTYFP10 := NULL]
+blu <- fread(file.path(data.dir, "all_blocks_usim_id.csv"), quote = "") %>% as.data.table()
+blu <- blu[, GEOID10 := str_extract(geoid, "\\d+")][, geoid := NULL]
+
+hhtots[blu, on = c("GEOID10"), `:=` (census_block_id = i.census_block_id)]
+
