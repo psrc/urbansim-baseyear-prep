48b727ae081875c81eccd932fc4633ad37a89bf7
cyyllam <cyyllam@gmail.com>
2022-08-09 15:08:52 -0700

    added post processing lockout codes

--- future_land_use/unroll_constraints.py
+++ future_land_use/unroll_constraints.py
@@ -6,7 +6,7 @@ from datetime import date
 
 # config ----------------------------------------------------
 
-exec(open(r'C:\Users\clam\Desktop\urbansim-baseyear-prep\future_land_use\unroll_constraints_functions.py').read())
+exec(open(r'C:\Users\CLam\github\urbansim-baseyear-prep\future_land_use\unroll_constraints_functions.py').read())
 
 pd.set_option('display.max_rows', 1000)
 pd.set_option('display.max_columns', 500)
@@ -19,7 +19,7 @@ dir = r"J:\Staff\Christy\usim-baseyear"
 flu_shp_path = r"W:\gis\projects\compplan_zoning\FLU_dissolve.shp"
 
 # imputed data
-flu_imp = os.path.join(dir, r'flu\final_flu_imputed_2021-06-17.csv')
+flu_imp = os.path.join(dir, r'flu\final_flu_postprocessed_2022-05-09.csv')
 
 # parcels file
 base_year_prcl_path = r"J:\Projects\2018_base_year\Region\prclpt18.shp"
@@ -32,12 +32,7 @@ f = pd.read_csv(flu_imp) # 1697 rows
 # clean up f; remove extra/unecessary fields before join
 f_col_keep = [col for col in f.columns if col not in ['Jurisdicti', 'Key', 'Zone_adj', 'Definition'] + list(f.columns[f.columns.str.endswith("src")])]
 f = f[f_col_keep]
-
-# assign plan_type_id
-f['plan_type_id'] = np.arange(len(f)) + 1
-
-# export f
-f.to_csv(os.path.join(dir, r'flu\flu_imputed_ptid_' + str(date.today()) + '.csv'), index=False)
+f['plan_type_id'] = np.arange(len(f)) + 1 # assign plan_type_id
 
 # join imputed data back to FLU shapefile
 flu = flu_shp.merge(f, on = ['Juris_zn'], how = 'left') # 1882 rows
@@ -46,23 +41,56 @@ flu = flu_shp.merge(f, on = ['Juris_zn'], how = 'left') # 1882 rows
 
 # spatial join parcels to flu to assign plan_type_id----------------------------------------------------
 
-# read parcels file
+# read parcels file & lu type file
 prcls = gpd.read_file(base_year_prcl_path)
+
+cache = r'N:\base_year_2018_inputs\gold_standard_inputs\2018\parcels'
+prcls_pin = np.fromfile(os.path.join(cache, 'parcel_id.li4'), np.int32)
+prcls_lu = np.fromfile(os.path.join(cache, 'land_use_type_id.li4'), np.int32)
+lu_type = pd.DataFrame({'PIN':prcls_pin, 'lu_type':prcls_lu}, index = prcls_pin)
+lu_type['PIN'] = lu_type['PIN'].astype(np.int64)
+
+prcls = prcls.merge(lu_type, on = 'PIN')
 
 prcls_flu = gpd.sjoin(prcls, flu)
 
 # QC FLU shapefile------------------------------------------------------------------
 
-check_multi_pins(prcls_flu, dir)
+#check_multi_pins(prcls_flu, dir) #check code
 
-# export table of parcels/ptid -----------------------------------------------------
-prcls_flu_subset = prcls_flu[['PIN', 'plan_type_id']] # preview
-prcls_flu_ptid = prcls_flu_subset[prcls_flu_subset['plan_type_id'].notna()] # remove NA
-prcls_flu_ptid.to_csv(os.path.join(dir, r'dev_constraints\prcls_ptid_' + str(date.today()) + '.csv'), index=False)
+# check for one-to-many records in flu overlay
+prcls_flu['PIN'].duplicated().any()
+duplicate = prcls_flu[prcls_flu.duplicated('PIN')][['PIN']]
+dup_df = prcls_flu[prcls_flu['PIN'].isin(duplicate['PIN'])].sort_values(by=['PIN'])
+#dup_nulls = dup_df[dup_df['plan_type_id'].isnull()]
 
-#prcls_flu.to_file(os.path.join(dir, r'shapes\prclpt18_ptid_' + str(date.today()) + '.shp'))
+#dup_df.to_csv(os.path.join(dir, r'flu_qc\pins_dup_'+ str(date.today()) +'.csv'), index=False) 
+#dup_df.to_file(os.path.join(dir, r'flu_qc\prcls_pins_dup_'+ str(date.today()) +'.shp'))
 
-# create development constraints table------------------------------------------------------------------
+# count frequency of PINs
+dup_pin_freq = dup_df.groupby(['PIN'])['PIN'].count().reset_index(name='counts')
+triple_pin = dup_pin_freq[dup_pin_freq['counts']>2] # handle triple count pins separately
+
+# re-assemble all parcels
+
+unjoined = prcls[~prcls['PIN'].isin(prcls_flu['PIN'])] # 4654 recs
+x1 = prcls_flu[~prcls_flu['PIN'].isin(dup_df['PIN'])] # no duplicates
+
+x2 = dup_df[~dup_df['plan_type_id'].isnull() & ~dup_df['PIN'].isin(triple_pin['PIN'])] # duplicates where plan_type_id is not null. Excludes triple_pin
+x2a = dup_df[dup_df['PIN'].isin(triple_pin['PIN']) & ~dup_df['plan_type_id'].isnull()] # triple_pin where plan_type_id is not null
+
+t_null = dup_df[dup_df['plan_type_id'].isnull() & ~dup_df['PIN'].isin(triple_pin['PIN'])] # duplicates amongst nulls. Excludes triple_pin
+t_dup_pin_freq = t_null.groupby(['PIN'])['PIN'].count().reset_index(name='count')
+t_dup_pin_freq_dups = t_dup_pin_freq[t_dup_pin_freq['count']>1]
+null_dup = t_null[t_null['PIN'].isin(t_dup_pin_freq_dups['PIN'])] 
+x3 = null_dup.drop_duplicates(subset=['PIN'], keep='first') # remove duplicates amonst nulls (take first or last)
+#len(x1) + len(x2) + len(x2a) + len(x3) + len(unjoined) # 1302434 recs
+
+# append all tables
+all_df = pd.concat([x1, x2, x2a, x3, unjoined])
+
+
+#### create development constraints table------------------------------------------------------------------
 
 # unroll constraints from plan_type
 id_cols = ['plan_type_id', 'generic_land_use_type_id', 'constraint_type']
@@ -139,5 +167,41 @@ devconstr.loc[devconstr['maxht'].isnull(), 'maxht'] = 0
 # add an id column
 devconstr['development_constraint_id']= np.arange(len(devconstr)) + 1
 
-devconstr.to_csv(os.path.join(dir, r'dev_constraints\devconstr_' + str(date.today()) + '.csv'), index=False) 
+# export files ---------------------------------------------------------------------
+#devconstr.to_csv(os.path.join(dir, r'dev_constraints\devconstr_' + str(date.today()) + '.csv'), index=False) 
 
+#f.to_csv(os.path.join(dir, r'flu\flu_imputed_ptid_' + str(date.today()) + '.csv'), index=False) # flu imputed kitchen sink file
+
+prcls_flu_ptid = all_df[['PIN', 'plan_type_id']]
+#prcls_flu_ptid.to_csv(os.path.join(dir, r'dev_constraints\prcls_ptid_' + str(date.today()) + '.csv'), index=False)
+#all_df.to_file(os.path.join(dir, r'shapes\prclpt18_ptid_' + str(date.today()) + '.shp'))
+
+# post-processing lockouts ----------------------------------------------------------
+
+# append to development constraints
+lo_df = pd.DataFrame()
+for x in range(50001, 50008):
+    lockout_ptid_df = pd.DataFrame({'plan_type_id': np.repeat(x, 7),
+                  'generic_land_use_type_id': list(np.arange(1, 7)) + [6],
+                  'minimum': 0,
+                  'maximum': 0,
+                  'lc': 1,
+                  'constraint_type': list(np.repeat("units_per_acre", 2)) + list(np.repeat("far", 4)) + ["units_per_acre"]})
+    if lo_df.empty:
+        lo_df = lockout_ptid_df
+    else:
+        lo_df = pd.concat([lo_df, lockout_ptid_df])
+
+# update plan_type_ids
+all_df.loc[all_df['plan_type_id'].isnull(), 'plan_type_id'] = 9999
+all_df.loc[all_df['lu_type'] == 23, 'plan_type_id'] = 50001 # Schools/universities
+all_df.loc[all_df['lu_type'] == 7, 'plan_type_id'] = 50002 # Government
+all_df.loc[all_df['lu_type'] == 9, 'plan_type_id'] = 50003 # Hospitals, convalescent center
+all_df.loc[all_df['lu_type'] == 6, 'plan_type_id'] = 50004 # Forest, protected
+all_df.loc[all_df['lu_type'] == 5, 'plan_type_id'] = 50005 # Forest, harvestable
+all_df.loc[all_df['lu_type'] == 1, 'plan_type_id'] = 50006 # Agriculture
+all_df.loc[all_df['lu_type'] == 27, 'plan_type_id'] = 50007 # Vacant undevelopable
+
+# export post-processing lockouts version
+prcls_flu_ptid_lockouts = all_df[['PIN', 'plan_type_id']]
+#prcls_flu_ptid_lockouts.to_csv(os.path.join(dir, r'dev_constraints\prcls_ptid_lockouts_' + str(date.today()) + '.csv'), index=False)
