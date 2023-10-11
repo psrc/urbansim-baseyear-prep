1f70a2d939e685e28da6b85458713f44e18f8334
Hana Sevcikova <hanas@uw.edu>
2023-10-10 18:33:48 -0700

    BY2023 Kitsap script

---
+++ collect_parcels_buildings/BY2023/process_Kitsap.R
@@ -0,0 +1,234 @@
+# Script to create parcels and buildings tables from Kitsap assessor data
+# for the use in urbansim 
+#
+# Hana Sevcikova, last update 10/10/2023
+#
+
+library(data.table)
+
+county <- "Kitsap"
+
+data.dir <- file.path("~/e$/Assessor23", county) # path to the Assessor text files
+misc.data.dir <- "data" # path to the BY2023/data folder
+write.result.to.mysql <- TRUE # it will overwrite the existing tables parcels_urbansim & buildings_urbansim
+
+if(write.result.to.mysql) source("mysql_connection.R")
+
+###################
+# Process parcels
+###################
+
+# load parcels tables
+parcels <- fread(file.path(data.dir, "parcels.txt"))
+parcels.23to18 <- fread(file.path(data.dir, "parcels23_kit_to_2018_parcels.csv"))
+
+# remove duplicates (just take the first record of each duplicate)
+parcels.nodupl <- parcels[!duplicated(rp_acct_id)]
+parcels.23to18.nodupl <- parcels.23to18[!duplicated(rp_acct_id)]
+cat("\nNumber of duplicates removed from parcels: ", nrow(parcels) - nrow(parcels.nodupl), "\n")
+
+# merge parcels
+parcels_allinfo <- merge(parcels.nodupl, 
+                        parcels.23to18.nodupl[, .(rp_acct_id, pin, point_x, point_y, shape_area)],
+                        by = "rp_acct_id")
+
+# load tables flatats, land & main
+tax <- fread(file.path(data.dir, "flatats.txt")) # Combined Tax Account Data
+land <- fread(file.path(data.dir, "land.txt")) # Land data
+main <- fread(file.path(data.dir, "main.txt")) # Real Property Information
+
+# remove any duplicates
+tax <- unique(tax)
+land <- unique(land)
+main <- unique(main)
+
+# extract what is needed and merge together with parcels
+# flatats dataset
+prep_parcels <- merge(parcels_allinfo,
+                      tax[, .(rp_acct_id, levy_code, jurisdict, acres_tax = acres, bldg_value, 
+                              land_value, assd_value)],
+                      by = "rp_acct_id", all.x = TRUE)
+# There are more than 42K parcels in the "tax" dataset that are not in "parcels",
+# but only 324 of them have acres > 0. We will ignore them for now.
+
+# land dataset
+prep_parcels <- merge(prep_parcels,
+                      land[, .(rp_acct_id, acres_land = acres, nbrhd_cd, prop_class, 
+                               zone_code, num_dwell, num_other, tot_improv)],
+                      by = "rp_acct_id", all.x = TRUE)
+cat("\nNumber of records in land that are not available in parcels: ",
+    nrow(prep_parcels[!rp_acct_id %in% land[, rp_acct_id]]), "\n")
+
+# main dataset
+prep_parcels <- merge(prep_parcels,
+                      main[, .(rp_acct_id, acct_stat, tax_status, tax_year)],
+                      by = "rp_acct_id", all.x = TRUE)
+# There are 427 records in parcels that are not present in land and main.
+cat("\nNumber of records in main that are not available in parcels: ",
+    nrow(prep_parcels[!rp_acct_id %in% main[, rp_acct_id]]), "\n")
+
+# check for duplicates
+cat("\nNumber of duplicates: ", nrow(prep_parcels[, .N, by = "rp_acct_id"][N > 1]), "\n")
+
+parcels_final <- prep_parcels[, .(parcel_id_fips = rp_acct_id, 
+                                  parcel_id = pin, land_value, num_dwell,
+                                  use_code = as.character(prop_class),
+                                  exemption = tax_status != "T", 
+                                  gross_sqft = shape_area, 
+                                  y_coord_sp = point_y, x_coord_sp = point_x
+                                  )]
+# TODO: - Do we need the other columns of prep_parcels? 
+#.      - Does the dataset need all the other columns at this point?
+
+# load land use reclass table
+lu_reclass <- fread(file.path(misc.data.dir, "land_use_generic_reclass_2018.csv"))
+
+parcels_final[lu_reclass[county_id == 35], land_use_type_id := i.land_use_type_id, 
+              on = c(use_code = "county_land_use_code")]
+cat("\nThe following codes were not found:")
+print(parcels_final[is.na(land_use_type_id) & !is.na(use_code), .N, by = "use_code"][order(use_code)])
+
+
+###################
+# Process buildings
+###################
+
+# load assessor buildings
+# ------------------------
+raw_buildings <- fread(file.path(data.dir, "building.txt"))
+
+prep_buildings <- raw_buildings[, .(id = 1:nrow(raw_buildings),
+                                    improv_type = improv_typ, 
+                                    rp_acct_id, total_sqft = flr_tot_sf,
+                                    year_built, stories, bedrooms,
+                                    bldg_type = bldg_typ, use_desc
+                                    )]
+
+# add mobile homes
+# ------------------
+mobile_homes <- fread(file.path(data.dir, "mh_fixed.txt"))[, V15 := NULL] # remove extra column
+# remove duplicates
+mobile_homes.nodupl <- mobile_homes[!duplicated(mobile_homes[, c("rp_acct_id", "yr_blt", "mh_make", "mh_size"), with = FALSE])]
+cat("\nNumber of duplicates removed from mobile homes: ", nrow(mobile_homes) - nrow(mobile_homes.nodupl), "\n")
+
+# identify building records for these mobile homes
+bldg_mh <- merge(mobile_homes.nodupl, prep_buildings[, .(id_bld = id, rp_acct_id, yr_blt = year_built)], 
+                 by = c("rp_acct_id", "yr_blt"), all.x = TRUE)
+# if year didn't match try without it, but blg_type must be MHOME
+rp <- unique(bldg_mh[is.na(id_bld) & rp_acct_id %in% prep_buildings[bldg_type == "MHOME", rp_acct_id], rp_acct_id])
+bldg_mh[prep_buildings[bldg_type == "MHOME" & rp_acct_id %in% rp], 
+        `:=`(id_bld = i.id, yr_blt = pmax(i.year_built, yr_blt)), on = "rp_acct_id"] # take the max of year built
+
+# For now we will ignore the 14 remaining mobile homes where there is no match to the buildings
+
+cat("\nIgnoring ", nrow(bldg_mh[is.na(id_bld)]), " mobile homes with no match to the buildings dataset.")
+
+# update total sqft and year built for mobile homes
+prep_buildings[bldg_mh, `:=`(total_sqft = i.mh_size, year_built = i.yr_blt), on = c(id = "id_bld")]
+
+
+# Calculate improvement value
+# ----------------------------
+# It will be distributed to individual buildings proportionally to the sqft
+valuation <- fread(file.path(data.dir, "Valuations.txt"))
+
+year_valuation <- valuation[tax_yr == 2023]
+# if property doesn't have valuation for 2023, take the 2024 value
+year_valuation <- rbind(year_valuation, 
+                        valuation[!rp_acct_id %in% year_valuation[, rp_acct_id] & tax_yr == 2024])
+cat("\nNumber of missed records: ", nrow(year_valuation[!rp_acct_id %in% valuation[, rp_acct_id]]))
+cat("\nNumber of duplicates: ", nrow(year_valuation[duplicated(rp_acct_id)]))
+
+# calculate proportions of sqft. Set minimum of 1 for zero sqft
+prep_buildings[, `:=`(total_sqft_tmp = pmax(1, total_sqft))]
+# sum total_sqft per rp_acct_id
+prep_buildings[, `:=`(total_sqft_per_account = sum(total_sqft_tmp)), by = "rp_acct_id"]
+# calculate proportions and resulting improvement value
+prep_buildings[, proportion := total_sqft_tmp / total_sqft_per_account
+               ][year_valuation, improvement_value := round(proportion * i.impr_av), 
+                 on = "rp_acct_id"]
+
+# impute DUs
+# ------------
+# drop buildings that do not have spatial representation in parcels
+nbld <- nrow(prep_buildings)
+prep_buildings_in_pcl <- prep_buildings[rp_acct_id %in% parcels_final[, parcel_id_fips]]
+cat("\nDropped ", nbld - nrow(prep_buildings_in_pcl), " buildings due to missing parcels.")
+
+# join with parcel info 
+prep_buildings_in_pcl <- merge(prep_buildings_in_pcl, 
+                               parcels_final[, .(parcel_id_fips, parcel_id, num_dwell, land_use_type_id, use_code)],
+                               by.x = "rp_acct_id", by.y = "parcel_id_fips",
+                               all.x = TRUE)
+prep_buildings_in_pcl[is.na(num_dwell), num_dwell := 0]
+prep_buildings_in_pcl[improv_type == "DWELLING", residential_units := 1][, residential_units_orig := 1]
+prep_buildings_in_pcl[is.na(residential_units), `:=`(residential_units = 0, residential_units_orig = 0)]
+prep_buildings_in_pcl[improv_type == "DWELLING", `:=`(sum_du = sum(residential_units)), by = "parcel_id"]
+
+# Correct units for multiplexes 
+codes_units <- list(`121` = 2,   # Duplex
+                    `122` = 3,   # Triplex
+                    `123` = 4,   # Fourplex
+                    `131` = c(5, 9, 7),   # 5-9 units (min, max, default)
+                    `132` = c(10, 14, 12),  # 10-14 units
+                    `133` = c(15, 19, 17),  # 15-19 units
+                    `134` = c(20, 29, 25),  # 20-29 units
+                    `135` = c(30, 39, 35),  # 30-39 units
+                    `136` = c(40, 49, 45),  # 40-49 units
+                    `137` = c(50, 100, 60), # 50+. (min, max, default)
+                    `141` = c(4, 100, 20)  # Condo (min, max, default)
+                    )
+# first process parcels that have only 1 unit
+for(code in names(codes_units)){
+    index <- with(prep_buildings_in_pcl, improv_type == "DWELLING" & sum_du == 1 & bldg_type != "MHOME" & 
+                      !is.na(use_code) & use_code == code)
+    if(sum(index) == 0) next # if no buildings for this code, go to the next code
+    if(length(codes_units[[code]]) > 1){ # number of units is given as a range; compute from sqft
+        units <- prep_buildings_in_pcl[index, round(total_sqft/1000)] # using 1000sf per unit 
+        units <- pmax(codes_units[[code]][1], pmin(units, codes_units[[code]][2])) # keep it in the given range
+    } else units <- codes_units[[code]]
+    prep_buildings_in_pcl[index,  residential_units := units]
+}
+
+# now process parcels with multiple buildings
+# here correct units for duplexes 
+for(code in names(codes_units)) {
+    prep_buildings_in_pcl[improv_type == "DWELLING" & sum_du > 1 & sum_du < codes_units[[code]][1] & 
+                              !is.na(use_code) & use_code == code & use_desc == "Duplex", 
+                          residential_units := 2]
+}
+# now we impute units into Multi-family records 
+# recompute the number of DUs per parcel and get the number of Multi-family records
+prep_buildings_in_pcl[, `:=`(sum_du2 = sum(residential_units), nmf = sum(use_desc == "Multi-family")), 
+                      by = "parcel_id"]
+for(code in names(codes_units)) {
+    # only MF records where the new sum of DUs hasn't reach what we expect
+    index <- with(prep_buildings_in_pcl, improv_type == "DWELLING" & sum_du2 > 1 & sum_du2 < codes_units[[code]][1] & 
+                      !is.na(use_code) & use_code == code & nmf > 0 & use_desc == "Multi-family")
+    if(sum(index) == 0) next # if no buildings for this code, go to the next code
+    target <- if(length(codes_units[[code]]) > 1) codes_units[[code]][3] else codes_units[[code]] # number of DUs we expect according to the use code
+    # add the remainder of the expectation to the MF records 
+    prep_buildings_in_pcl[index, residential_units := residential_units + round((target - sum_du2) / nmf)]
+}
+
+cat("\nImputed", prep_buildings_in_pcl[, sum(residential_units) - sum(residential_units_orig)], 
+    "residential units. Total number of units is", prep_buildings_in_pcl[, sum(residential_units)],
+    ".")
+
+# collect attributes for the final table
+buildings_final <- prep_buildings_in_pcl[, .(building_id = id, gross_sqft = total_sqft, year_built,
+                                             parcel_id, rp_acct_id, residential_units, improvement_value,
+                                             non_residential_sqft = ifelse(residential_units > 0, 0, total_sqft),
+                                             use_code, use_desc)]
+
+# write results
+fwrite(parcels_final, file = "parcels_urbansim.csv")
+fwrite(buildings_final, file = "buildings_urbansim.csv")
+
+if(write.result.to.mysql){
+    db <- paste0(tolower(county), "_2023_parcel_baseyear")
+    connection <- mysql.connection(db)
+    dbWriteTable(connection, "parcels_urbansim", parcels_final, overwrite = TRUE, row.names = FALSE)
+    dbWriteTable(connection, "buildings_urbansim", buildings_final, overwrite = TRUE, row.names = FALSE)
+    DBI::dbDisconnect(connection)
+}
