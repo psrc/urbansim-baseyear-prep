182d0a2a31f6ed4bc4558f9697cbb1004276c929
Hana Sevcikova <hsevcikova@psrc.org>
2024-01-23 14:45:19 -0800

    snohomish processing

--- collect_parcels_buildings/BY2023/process_snohomish.R
+++ collect_parcels_buildings/BY2023/process_snohomish.R
@@ -15,7 +15,7 @@ county.id <- 61
 #data.dir <- file.path("~/e$/Assessor23", county) # path to the Assessor text files
 data.dir <- "Snohomish_data" # Hana's local path
 misc.data.dir <- "data" # path to the BY2023/data folder
-write.result <- FALSE # it will overwrite the existing tables urbansim_parcels & urbansim_buildings
+write.result <- TRUE # it will overwrite the existing tables urbansim_parcels & urbansim_buildings
 
 if(write.result) source("mysql_connection.R")
 
@@ -38,7 +38,10 @@ lu_reclass <- fread(file.path(misc.data.dir, "land_use_generic_reclass_2023.csv"
 bt_reclass <- fread(file.path(misc.data.dir, "building_use_generic_reclass_2023.csv"))
 
 # buildings
-raw_buildings <- fread(file.path(data.dir, "improvement.csv"))
+raw_buildings <- fread(file.path(data.dir, "improvement.txt"))
+
+# RVs to remove
+rvs <- fread(file.path(data.dir, "snoh_rv_units.csv"))
 
 ###################
 # Process parcels
@@ -60,13 +63,21 @@ parcels.23to18[, `:=`(new_parcel_id = as.character(pin23), parcel_id = as.charac
 full.parcels[, parcel_id := as.character(parcel_id)]
 exemptions[, parcel_id := as.character(parcel_number)]
 
+# remove records that are not in full.parcels (should be zero!)
+if(nrow((miss <- parcels.23to18[! parcel_id %in% full.parcels[, parcel_id]])) > 0){
+    warning("The following parcels are not contained in the full set of parcels: ", 
+            paste(miss[, parcel_id], collapse = ", "))
+    parcels.23to18 <- parcels.23to18[!parcel_id %in% miss[, parcel_id]]
+}
+
 # set base_pin_flag for all records
 parcels.23to18[, `:=`(Npcl = .N, Nbase = sum(base_pin_flag)), by = "new_parcel_id"]
 parcels.23to18[Npcl == 1 & Nbase == 0, `:=`(base_pin_flag = 1, Nbase = 1)]
-parcels.23to18[Nbase == 0 & new_parcel_id > 0, base_pin_flag := c(1, base_pin_flag[-1]), by = "new_parcel_id"] # set the flag to one for each first row in the group
+parcels.23to18[Nbase == 0 & new_parcel_id > 0, 
+               base_pin_flag := c(1, base_pin_flag[-1]), by = "new_parcel_id"] # set the flag to one for each first row in the group
 
 full.parcels[parcels.23to18, `:=`(new_parcel_id = i.new_parcel_id), on = "parcel_id"]
-exemptions[parcels.23to18, base_parcel := i.base_parcel, on = "parcel_id"]
+exemptions[parcels.23to18, new_parcel_id := i.new_parcel_id, on = "parcel_id"]
 full.parcels[, exemption := parcel_id %in% exemptions[, parcel_id]]
 
 # join with reclass table
@@ -80,11 +91,14 @@ if(nrow(misslu <- full.parcels[is.na(land_use_type_id) & !is.na(usecode), .N, by
     print(misslu[order(usecode)])
 } else cat("\nAll land use codes matched.")
 
+# assign land use type of the base parcel
 full.parcels[is.na(land_use_type_id), land_use_type_id := 0]
+parcels.23to18[full.parcels, land_use_type_id := i.land_use_type_id, on = "parcel_id"]
 full.parcels[parcels.23to18[base_pin_flag == 1], `:=`(base_land_use_type_id = i.land_use_type_id), 
              on = "new_parcel_id"]
-full.parcels[, `:=`(N = .N, has_valid_land_use_type = any(land_use_type_id > 0)), by = "base_parcel"]
-full.parcels[base_land_use_type_id == 0 & N > 1, use_code_base_parcel := ifelse(has_valid_use_code, NA, use_code)]
+full.parcels[is.na(base_land_use_type_id), base_land_use_type_id := ifelse(is.na(land_use_type_id), 0, land_use_type_id)]
+full.parcels[, `:=`(N = .N, has_valid_base_land_use_type = any(base_land_use_type_id > 0)), by = "new_parcel_id"]
+full.parcels[base_land_use_type_id == 0 & N > 1, base_land_use_type_id := ifelse(has_valid_base_land_use_type, NA, land_use_type_id)]
 
 
 # aggregate land value
@@ -92,9 +106,19 @@ prep_parcels <- full.parcels[!is.na(parcel_id), .(land_value = sum(mklnd),
                                                     improvement_value = sum(mkimp),
                                                     total_value = sum(mkttl),
                                                     exemption = as.integer(any(exemption)),
-                                                    land_use_type_id = land_use_type_id[1]
-                                                    ), by = "base_parcel"] 
+                                                    land_use_type_id = max(base_land_use_type_id, na.rm = TRUE),
+                                                    use_code_dif = any(land_use_type_id != max(base_land_use_type_id, na.rm = TRUE)),
+                                                    N = .N
+                                                    ), by = "new_parcel_id"] 
 prep_parcels[is.na(land_use_type_id), land_use_type_id := 0]
+# for now we use the land use type of the base parcel
+# but these parcels have differences in land use types:
+prep_parcels[!is.na(new_parcel_id) & new_parcel_id > 0 & N > 1 & use_code_dif == TRUE][order(-N)]
+
+# get sqft and x & y coordinates
+prep_parcels[parcels.23to18[base_pin_flag == 1], `:=`(pin = i.pin, 
+        gross_sqft = i.gis_sqft, x_coord_sp = i.point_x_int, y_coord_sp = i.point_y_int),
+             on = "new_parcel_id"]
 
 # prep_parcels <- parcels.nodupl[, .(parcel_id = pin, parcel_number = parcel_id, 
 #                                    parcel_lrsn = lrsn,
@@ -109,7 +133,10 @@ prep_parcels[is.na(land_use_type_id), land_use_type_id := 0]
 
 # construct final parcels 
 # (if no additional columns or other cleaning needed then it's just a copy of prep_parcels)
-parcels_final <- copy(prep_parcels)[, county_id := county.id]
+parcels_final <- prep_parcels[, .(parcel_id = pin, parcel_number = new_parcel_id, 
+                                  land_value, improvement_value, total_value, gross_sqft,
+                                  x_coord_sp, y_coord_sp, land_use_type_id, exemption,
+                                  county_id = county.id)]
 
 cat("\nTotal all:", nrow(parcels_final), "parcels")
 cat("\nAssigned to 2018:", nrow(parcels_final[!is.na(parcel_id) & parcel_id != 0]), "parcels")
@@ -125,23 +152,34 @@ cat("\n\nProcessing Snohomish buildings\n=========================\n")
 colnames(raw_buildings) <- tolower(colnames(raw_buildings))
 
 prep_buildings <- raw_buildings[, .(building_id = 1:nrow(raw_buildings),
-                                    parcel_number = pin, lrsn,
+                                    parcel_number_orig = as.character(pin), 
                                     imprtype, usecode, usedesc,
                                     bldgtype, stories, yrbuilt, finsize,
                                     numberrooms, numbedrms, propext)]
+
+# assign new_parcel_id
+prep_buildings[parcels.23to18, `:=`(new_parcel_id = i.new_parcel_id, 
+                                    land_use_type_id = i.land_use_type_id),
+                                    on = c(parcel_number_orig = "parcel_id")]
 
 # join with building reclass table
 prep_buildings[bt_reclass[county_id == county.id], building_type_id := i.building_type_id, 
                on = c(usecode = "county_building_use_code")]
 
-# Calculate improvement value proportionally to the sqft
-prep_buildings[prep_parcels, `:=`(total_improvement_value = i.improvement_value, 
-                                  parcel_id = i.parcel_id, land_use_type_id = i.land_use_type_id),
-               on = "parcel_number"]
+
+# Calculate improvement value proportionally to the sqft (using the original parcels)
+prep_buildings[full.parcels, `:=`(total_improvement_value = i.mkimp, 
+                                  pin = i.pin),
+               on = c(parcel_number_orig = "parcel_id")]
 prep_buildings[, `:=`(sqft_tmp = pmax(1, finsize, na.rm = TRUE))]
-prep_buildings[, `:=`(total_sqft = sum(sqft_tmp), count = .N), by = "parcel_number"]
+prep_buildings[, `:=`(total_sqft = sum(sqft_tmp), count = .N), by = "parcel_number_orig"]
 prep_buildings[, `:=`(improvement_value = round(sqft_tmp/total_sqft * total_improvement_value))]
 
+# remove RVs in three areas: Port Susan, Gold Bar, Lake Connor
+nbld <- nrow(prep_buildings)
+prep_buildings <- prep_buildings[! parcel_number_orig %in% rvs[, as.character(pin23_orig)]]
+cat("\n", nbld - nrow(prep_buildings), " RVs removed.\n")
+               
 # As there are no mobile homes in the reclass table,
 # set building type to mobile home if on mobile home land use type and if residential use code
 prep_buildings[land_use_type_id == 13 & 
@@ -190,16 +228,16 @@ if(nrow(missbt <- prep_buildings[is.na(building_type_id), .N, by = c("usecode", 
 # TODO: for non-res buildings is gross_sqft the same as non_residential_sqft?
 buildings_final <- rbind(
     prep_buildings[building_type_id %in% c(4, 12, 19, 11)
-    , .(building_id, parcel_number, building_type_id, gross_sqft = finsize, 
+    , .(building_id, parcel_number = new_parcel_id, building_type_id, gross_sqft = finsize, 
         sqft_per_unit = round(finsize/residential_units),
         year_built = yrbuilt, residential_units, non_residential_sqft = 0,
-        improvement_value, use_code = usecode, stories, parcel_lrsn = lrsn
+        improvement_value, use_code = usecode, stories
         )],
     prep_buildings[! building_type_id %in% c(4, 12, 19, 11)
-    , .(building_id, parcel_number, building_type_id, gross_sqft = finsize, 
+    , .(building_id, parcel_number = new_parcel_id, building_type_id, gross_sqft = finsize, 
         sqft_per_unit = 1, year_built = yrbuilt, residential_units = 0,
         non_residential_sqft = finsize, improvement_value, use_code = usecode, 
-        stories, parcel_lrsn = lrsn
+        stories
         )])
 # add urbansim parcel_id
 buildings_final[parcels_final, parcel_id := i.parcel_id, on = "parcel_number"]
@@ -212,11 +250,23 @@ cat("\nDropped ", nbld - nrow(buildings_final), " buildings due to missing parce
 # rename parcel_number column
 setnames(buildings_final, "parcel_number", "parcel_id_fips") 
 # column order
-setcolorder(buildings_final, c("building_id", "parcel_id", "parcel_id_fips", "parcel_lrsn"))
+setcolorder(buildings_final, c("building_id", "parcel_id", "parcel_id_fips"))
 
 # remove columns from parcels_final and rename parcel_number
 parcels_final[, `:=`(improvement_value = NULL, total_value = NULL)]
 setnames(parcels_final, "parcel_number", "parcel_id_fips") 
+
+# replace NAs with zeros
+for(col in c("gross_sqft", "year_built", "non_residential_sqft")){
+    buildings_final[[col]][is.na(buildings_final[[col]])] <- 0
+}
+
+# clean stories column
+# check with buildings_final[, .N, by = "stories"]
+buildings_final[stories %in% c("5353", "53", "053", "2022", "", "\x80P@"), stories := NA]
+buildings_final[stories %in% c("2.,0", "2+", "2..0", "2.-0", "2,0"), stories := "2"]
+buildings_final[stories == "1,5", stories := "1.5"]
+buildings_final[, stories := as.numeric(stories)]
 
 cat("\nTotal all: ", nrow(buildings_final), "buildings")
 cat("\nAssigned to 2018:", nrow(buildings_final[!is.na(parcel_id) & parcel_id != 0]), "buildings")
